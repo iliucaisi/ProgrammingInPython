ac9cb117176fe6cd020dd606d9b8f79212f574be
LIU,Caisi <caisi.liu@nokia-sbell.com>
2017-08-10 18:01:03 +0530

    Update colorful output.

--- modules/click/demo_1.py
+++ modules/click/demo_1.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import click
 import math
+import colorama
 
 @click.command()
 @click.argument('x1')
@@ -9,8 +10,8 @@ import math
 @click.argument('y2')
 def distance(x1, y1, x2, y2):
 	"""Calculate the distance between (x1, y1) and (x2, y2)"""
-	click.echo('The distance between (%s, %s) and (%s, %s) is: ' % (x1, y1, x2, y2))
-	click.echo('%s' % (math.sqrt((float(y2) - float(y1))**2 + (float(x2) - float(x1))**2)))
+	click.secho('The distance between (%s, %s) and (%s, %s) is: ' % (x1, y1, x2, y2), bg='white', fg='black', bold=True)
+	click.secho('%s' % (math.sqrt((float(y2) - float(y1))**2 + (float(x2) - float(x1))**2)), bg='white', fg='red', dim=True)
 
 if __name__ == '__main__':
 	distance()
